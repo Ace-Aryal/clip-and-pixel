e863e21355e8ec054c0ce9073d4acc01b23a0829
Dipesh Aryal <aryaldeelep@gmail.com>
2025-07-04 16:43:27 +0545

    set up auth

---
+++ app/_components/navbar.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between px-6 py-3 shadow-sm border-b border-gray-300">
+      <div className="text-2xl font-bold">Clips & Pixels</div>
+      <div>
+        <SignedOut>
+          <SignInButton>
+            <Button variant="blue">Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
---
+++ app/social-share/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SocaialSharePage() {
+  return <div>SocaialSharePage</div>;
+}
+
+export default SocaialSharePage;
